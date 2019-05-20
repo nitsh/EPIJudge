49c672af3471a9a574b1383f318ea7afc248262b
Nitish Alluri <nitish.alluri@gmail.com>
2019-05-20 11:49:54 +0700

    parity solution

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,11 @@ from test_framework import generic_test
 
 
 def parity(x):
-    # TODO - you fill in here.
-    return 0
+    result = 0
+    while x != 0:
+        result += (x & 1)
+        x >>= 1
+    return result % 2
 
 
 if __name__ == '__main__':
