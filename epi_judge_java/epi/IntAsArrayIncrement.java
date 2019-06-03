8805b13d89781e1011f7d04743837047d0728b3a
Nitish Alluri <nitish.alluri@gmail.com>
2019-06-03 22:56:37 +0700

    int as array increment finished

--- epi_judge_java/epi/IntAsArrayIncrement.java
+++ epi_judge_java/epi/IntAsArrayIncrement.java
@@ -1,12 +1,26 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
 import java.util.List;
 public class IntAsArrayIncrement {
   @EpiTest(testDataFile = "int_as_array_increment.tsv")
   public static List<Integer> plusOne(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
+      Integer carryOver = 1;
+      for(int i = A.size() - 1; i >= 0; i--){
+          if (A.get(i) + carryOver == 10) {
+              A.set(i, 0);
+          } else {
+              A.set(i, A.get(i) + carryOver);
+              carryOver = 0;
+              break;
+          }
+      }
+      if(carryOver == 1) {
+          A.add(0, 1);
+      }
+      return A;
   }
 
   public static void main(String[] args) {
